a4f4c3f9f581bb0fa0a6f0619e1371fcb0aae86e
h66m9d <h66m9d@users.noreply.github.com>
2018-01-19 15:34:55 +0330

    Update install-on-ubuntu.sh

--- install-on-ubuntu.sh
+++ install-on-ubuntu.sh
@@ -1,45 +1,46 @@
-apt-get install deluge deluged deluge-console
-deluged
+apt-get install deluge deluged deluge-console -y
+killall deluged
 deluge-console plugin -e Execute
 
-cat > ~/tartorrent.sh <<EOL
+cat > ~/.config/deluge/tartorrent.sh <<EOL
 #!/bin/bash
-torrentid=$1
-torrentname=$2
-torrentnameunderline=`echo $2 | tr ' ' '_'`
-torrentpath=$3
+torrentid=\$1
+torrentname=\$2
+torrentnameunderline=\`echo \$2 | tr ' ' '_'\`
+torrentpath=\$3
 
 # recheck
-deluge-console recheck $torrentid
+deluge-console recheck \$torrentid
 
 # Go to torrent path
-cd $3
+cd \$3
 
 # make tarbal.gz package
-tar -czvf $torrentnameunderline.tar.gz "$torrentname"
+tar -czvf \$torrentnameunderline.tar.gz "\$torrentname"
 
 # remove torrent with data
-deluge-console rm --remove_data $torrentid
+deluge-console rm --remove_data \$torrentid
 
 # Rename to original name
 # make tarbal.gz package
-#mv $torrentnameunderline.tar.gz "$torrentname.tar.gz"
+#mv $torrentnameunderline.tar.gz "\$torrentname.tar.gz"
 EOL
 
-chmod 755 ~/tartorrent.sh
+chmod 755 ~/.config/deluge/tartorrent.sh
 
 cat > ~/.config/deluge/execute.conf <<EOL
 {
-  "file": 1,
+  "file": 1, 
   "format": 1
 }{
   "commands": [
-   [
-     "1",
-     "complete",
-     "~/tartorrent.sh"
-   ]
+    [
+      "0b58468a56042412948ec61c1697a6da9551aadd", 
+      "complete", 
+      "~/.config/deluge/tartorrent.sh"
+    ]
   ]
 }
 EOL
 
+deluged
