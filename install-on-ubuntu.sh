fb319cd7b38ef14960e32c18a34ebac81cd303d2
h66m9d <h66m9d@users.noreply.github.com>
2018-01-19 21:16:09 +0330

    Update install-on-ubuntu.sh

--- install-on-ubuntu.sh
+++ install-on-ubuntu.sh
@@ -10,21 +10,21 @@ torrentnameunderline=\`echo \$torrentname | tr ' ' '_'\`
 torrentpath=\$3
 
 # recheck and remove
-deluge-console recheck \$torrentid && echo \$(date +"%Y-%m-%d %H:%M:%S") -- \"\$torrentname\" recheck completed. >> \$torrentpath/debug.txt
-deluge-console rm \$torrentid && echo \$(date +"%Y-%m-%d %H:%M:%S") -- \"\$torrentname\" removed from deluge. >> \$torrentpath/debug.txt
+deluge-console recheck \$torrentid && echo \$(date +"%Y-%m-%d %H:%M:%S") -- \[\$torrentname\] recheck completed. >> \$torrentpath/debug.txt
+deluge-console rm \$torrentid && echo \$(date +"%Y-%m-%d %H:%M:%S") -- \[\$torrentname\] removed from deluge. >> \$torrentpath/debug.txt
 
 # Go to torrent path
 cd \$torrentpath
 
 # make tarbal.gz package
-tar -czvf .\$torrentnameunderline.tar.gz "\$torrentname" && echo \$(date +"%Y-%m-%d %H:%M:%S") -- \"\$torrentname\" packaging completed. >> \$torrentpath/debug.txt
+tar -czvf .\$torrentnameunderline.tar.gz "\$torrentname" && echo \$(date +"%Y-%m-%d %H:%M:%S") -- \[\$torrentname\] packaging completed. >> \$torrentpath/debug.txt
 rm -r "\$torrentname"
 rm "\$torrentname"
-echo \$(date +"%Y-%m-%d %H:%M:%S") -- \"\$torrentname\" removing cached files completed. >> \$torrentpath/debug.txt
+echo \$(date +"%Y-%m-%d %H:%M:%S") -- \[\$torrentname\] removing cached files completed. >> \$torrentpath/debug.txt
 
 # Rename to original name
 mv .\$torrentnameunderline.tar.gz "\$torrentname.tar.gz"
-echo \$(date +"%Y-%m-%d %H:%M:%S") -- \"\$torrentname\" package ready to download. >> \$torrentpath/debug.txt
+echo \$(date +"%Y-%m-%d %H:%M:%S") -- \[\$torrentname\] package ready to download. >> \$torrentpath/debug.txt
 EOL
 
 chmod 755 ~/.config/deluge/tartorrent.sh
