a70029cb98c70f5b337461987b00a5b2660ceb85
h66m9d <h66m9d@users.noreply.github.com>
2018-01-19 20:41:34 +0330

    Update install-on-ubuntu.sh

--- install-on-ubuntu.sh
+++ install-on-ubuntu.sh
@@ -10,20 +10,21 @@ torrentnameunderline=\`echo \$torrentname | tr ' ' '_'\`
 torrentpath=\$3
 
 # recheck and remove
-deluge-console recheck \$torrentid
-deluge-console rm \$torrentid
+deluge-console recheck \$torrentid && echo \$(date +"%Y-%m-%d %H:%M:%S") \$torrentname recheck completed.
+deluge-console rm \$torrentid && echo \$(date +"%Y-%m-%d %H:%M:%S") \$torrentname removed from deluge.
 
 # Go to torrent path
 cd \$torrentpath
-echo okay >> debug.txt
+
 # make tarbal.gz package
 tar -czvf .\$torrentnameunderline.tar.gz "\$torrentname"
 rm -r "\$torrentname"
 rm "\$torrentname"
+echo \$(date +"%Y-%m-%d %H:%M:%S") \$torrentname removing cached files completed.
 
 # Rename to original name
 mv .\$torrentnameunderline.tar.gz "\$torrentname.tar.gz"
-
+echo \$(date +"%Y-%m-%d %H:%M:%S") \$torrentname package ready to download.
 EOL
 
 chmod 755 ~/.config/deluge/tartorrent.sh
